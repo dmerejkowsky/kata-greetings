ce5bf12201a6306dc7aaf0287ff884a4b7b3bc4b
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-31 14:02:49 +0200

    First refactoring: separation of concerns

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-$(function () {
-  $('body').on('click', 'button.done', function () {
-    let name = $('input.name').val()
-    name = name.split(' ')
-    if (name[0] === 'Mr.' || name[0] === 'Mrs.' || name[0] === 'Dr.') {
-      name.shift()
-    }
-    $('#welcome').text('Welcome, ' + name[0] + '!')
-  })
-})
+$(setupClickHandlers)
+
+function setupClickHandlers () {
+  $('body').on('click', 'button.done', onDoneButtonClicked)
+}
+
+function onDoneButtonClicked () {
+  const name = getFullName()
+  const firstName = parseFirstName(name)
+  displayWelcome(welcomeMessage(firstName))
+}
+
+function getFullName () {
+  return $('input.name').val()
+}
+
+function parseFirstName (value) {
+  const name = value.split(' ')
+  if (name[0] === 'Mr.' || name[0] === 'Mrs.' || name[0] === 'Dr.') {
+    name.shift()
+  }
+  return name[0]
+}
+
+function welcomeMessage (firstName) {
+  return 'Welcome, ' + firstName + '!'
+}
+
+function displayWelcome (message) {
+  $('#welcome').text(message)
+}
